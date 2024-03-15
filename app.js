d39344f068fbf170561f8d9b977ea963f8490892
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-03-15 11:58:38 +0100

    Add entire game and forget to commit

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
-function getComputerChoice() {
-  let choiceRng = Math.floor(Math.random() * 3) + 1;
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
 
-  switch (choiceRng) {
-    case 1:
-      return "rock";
-    case 2:
-      return "paper";
-    case 3:
-      return "scissors";
-    default:
-      console.warn("choiceRng is an unexpected value! returning 'Rock'");
-      return "rock";
-  }
+let cpuChoice = "";
+let userChoice = "";
+
+function getComputerChoice() {
+	let choiceRng = Math.floor(Math.random() * 3) + 1;
+
+	switch (choiceRng) {
+		case 1:
+			return rock;
+		case 2:
+			return paper;
+		case 3:
+			return scissors;
+		default:
+			console.warn("choiceRng is an unexpected value! returning 'Rock'");
+			return rock;
+	}
+}
+
+function getUserChoice() {
+	let choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+	if (!(choice in [rock, paper, scissors])) {
+		alert("Incorrect choice!");
+		return null;
+	}
+
+	return choice;
+}
+
+function lose() {
+	alert("You lose! " + cpuChoice + " beats " + userChoice + "!");
+}
+
+function win() {
+	alert("You win! " + userChoice + " beats " + cpuChoice + "!");
+}
+
+userChoice = getUserChoice();
+if (userChoice === null) return;
+
+cpuChoice = getComputerChoice();
+
+console.log("CPU: " + cpuChoice);
+console.log("USER: " + userChoice);
+
+if (userChoice == cpuChoice) alert("It's a tie!");
+else if (userChoice == rock) {
+	if (cpuChoice == scissors) win();
+	else if (cpuChoice == paper) lose();
+} else if (userChoice == paper) {
+	if (cpuChoice == rock) win();
+	else if (cpuChoice == scissors) lose();
+} else if (userChoice == scissors) {
+	if (cpuChoice == paper) win();
+	else if (cpuChoice == rock) lose();
 }
